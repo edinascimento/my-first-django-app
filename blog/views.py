b0e5ab0bfd383f8db7eeb5f2eb22af1fb7092b4a
Edipo Nascimento <esnedipo@gmail.com>
2023-12-01 11:25:01 -0300

    added posts to the view template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-  return render(request, 'blog/post_list.html', {})    
+  posts = Post.objects.filter(author=1)
+  return render(request, 'blog/post_list.html', {
+    'posts': posts
+  })    
